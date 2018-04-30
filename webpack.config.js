32b4ed7b7b63ef963d0570e96d7b2cbd888dafd7
CandiW <candisw06@gmail.com>
2018-04-30 08:03:37 -0700

    fixed more issues with webpack.config.js file & excluding node_modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,53 @@
-module.exports = {
-  entry: "./routes/server.js",
-  output: {
-    path: __dirname + "/public",
-    filename: 'bundle.js'
+const webpack = require('webpack');
+
+const path = require('path');
+
+const nodeExternals = require('webpack-node-externals');
+
+const config = {
+
+  entry: {
+
+      app: './routes/server.js'
+
   },
-  module: {
-    loaders: [
+
+  output: {
+
+    path: __dirname + "/public",
+
+    filename: 'build/bundle.js'
+
+  },
+
+  module : {
+
+    rules : [
+
       {
-        test: /\.js$/,
-        loader: 'babel-loader'
+
+        test : /\.jsx?/,
+
+        loader : 'babel-loader',
+
+        exclude: /node_modules/,
+
+        query: {
+
+            "presets" : ["es2015", "react"]
+
+          }
+
       }
+
     ]
-  }
+
+  },
+
+  target: 'node',
+
+  externals: [nodeExternals()]
+
 };
+
+module.exports = config;
